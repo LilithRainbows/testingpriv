c2002a74f07aaec342c27defa79b3976ae02abb7
Lilith <44578053+LilithRainbows@users.noreply.github.com>
2022-01-08 11:03:40 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-document.documentElement.innerHTML = '';
-Probando
+document.documentElement.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/AZKcl4-tcuo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
